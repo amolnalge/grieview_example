2535b45c8051bbd248f829d4b1da47fb9009666b
amolnalge <amol.nalge719@gmail.com>
2014-12-24 16:44:18 +0530

    app is complete

--- GridViewAPP/src/com/example/gridviewapp/ImageAdapter.java
+++ GridViewAPP/src/com/example/gridviewapp/ImageAdapter.java
@@ -52,7 +52,7 @@ private Context mContext;
 		return imageView;
 		
 	}
-	public Integer[] mThumbIds={R.drawable.sample_0,R.drawable.sample_1,R.drawable.sample_2,R.drawable.sample_3,R.drawable.sample_4,R.drawable.sample_5,R.drawable.sample_6,R.drawable.sample_7,R.drawable.sample_3,R.drawable.sample_0};
+	public Integer[] mThumbIds={R.drawable.me,R.drawable.ic_launcher,R.drawable.sample_0,R.drawable.sample_1,R.drawable.sample_2,R.drawable.sample_3,R.drawable.sample_4,R.drawable.sample_5,R.drawable.sample_6,R.drawable.sample_7,R.drawable.sample_3,R.drawable.sample_0};
 	
 	
 	
