e5fb9296f3e7fb7b5e8dfb728096351f66b1d455
amolnalge <amol.nalge719@gmail.com>
2014-12-24 15:49:17 +0530

    Displayed images in GridView

--- GridViewAPP/src/com/example/gridviewapp/ImageAdapter.java
+++ GridViewAPP/src/com/example/gridviewapp/ImageAdapter.java
@@ -1,9 +1,13 @@
 package com.example.gridviewapp;
 
 import android.content.Context;
+import android.provider.MediaStore.Images;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
 private Context mContext;
@@ -15,8 +19,8 @@ private Context mContext;
 	}
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
-		return 0;
+	
+		return mThumbIds.length;
 	}
 
 	@Override
@@ -32,9 +36,21 @@ private Context mContext;
 	}
 
 	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
-		// TODO Auto-generated method stub
-		return null;
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		ImageView imageView;
+		if(convertView==null){
+			imageView=new ImageView(mContext);
+			imageView.setLayoutParams(new GridView.LayoutParams(150, 150));
+			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageView.setPadding(5, 5, 5, 5);
+			
+		}else{
+			imageView=(ImageView) convertView;
+		}
+		imageView.setImageResource(mThumbIds[position]);
+		return imageView;
+		
 	}
 	public Integer[] mThumbIds={R.drawable.sample_0,R.drawable.sample_1,R.drawable.sample_2,R.drawable.sample_3,R.drawable.sample_4,R.drawable.sample_5,R.drawable.sample_6,R.drawable.sample_7,R.drawable.sample_3,R.drawable.sample_0};
 	
